76b713b80e9545be81fd8b34133562d051e6f564
Narek Sahakyan <sahNarek>
2023-04-01 04:19:15 +0400

    add caching with redis

--- app.py
+++ app.py
@@ -11,11 +11,9 @@ from pymongo import MongoClient
 import jwt
 from functools import wraps
 from datetime import datetime
-import math
-import socket
-
-
-# HUGE TODO BREAK EVERYTHING INTO SMALLER MODULES
+from flask_caching import Cache
+import redis
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
@@ -26,6 +24,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 MONGO_URI = os.environ.get('MONGO_URI')
 cluster = MongoClient(MONGO_URI)
+cache = Cache(app, config={'CACHE_TYPE': 'redis', 'CACHE_REDIS_URL': 'redis://localhost:6379/0'})
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 mongo_db = cluster['e-commerece-app-db']
 products_collection = mongo_db.products
@@ -33,14 +33,45 @@ orders_collection = mongo_db.orders
 sessions_collection = mongo_db.sessions
 requests_collection = mongo_db.requests
 logs_collection = mongo_db.server_logs
-# Add mongo response handling and logging
-
 
 from models import User
 
 
 def get_product_by_id(id):  
     return products_collection.find_one({"_id": ObjectId(id)})
+
+def get_cached_products():
+    products_list = redis_client.get("products")
+
+    if products_list is not None:
+        return json.loads(redis_client.get('products').decode('utf-8'))
+
+    return None
+
+
+def update_cached_products(id, update_data):
+    cached_products = get_cached_products()
+    if cached_products is None:
+        return None
+    for cached_product in cached_products:
+        if cached_product["id"] == id:
+            cached_product.update(update_data)
+
+    redis_client.set("products", json.dumps(cached_products))
+
+def delete_product_from_cache(id):
+    cached_products = get_cached_products()
+    filtered_products = [product for product in cached_products if product["id"] != id]
+    redis_client.set("products", json.dumps(filtered_products))
+
+def find_product_from_cache(attribute,value):
+    cached_products = get_cached_products()
+    if cached_products is None:
+        return None
+    for cached_product in cached_products:
+        if cached_product[attribute] == value:
+            return cached_product
+    return None
 
 def log_request(f):
     @wraps(f)
@@ -148,21 +179,23 @@ def login():
 @app.route("/products", methods=["GET"])
 @log_request
 def get_products():
-    products_list = []
+    cached_products = get_cached_products()
+    if cached_products is not None:
+        return jsonify(cached_products)
 
+    products_list = []
     mongo_products = products_collection.find()
 
     for item in mongo_products:
-
         product_dict = {
             "id": str(item['_id']),
             "name"    : str(item['name']),
             "price"   : int(item['price']),
             "in_stock_quantity": int(item['in_stock_quantity'])
         }
-
         products_list.append(product_dict)
-    
+        redis_client.set('products', json.dumps(products_list))
+
     return jsonify(products_list)
 
 
@@ -172,10 +205,8 @@ def get_products():
 def add_to_cart(current_user):
     try:
         item = request.json['item']
-
         sessions_collection.update_many({'public_id': current_user.email}, 
                                         {'$addToSet': { 'items_added': item } })
-
         return jsonify({"message": "Item added to the cart"}, 200)
         
     except Exception as e:
@@ -220,25 +251,42 @@ def checkout(current_user):
 @log_request
 def add_product():
     try:
-        product = request.json["product"]
-        if products_collection.find_one({'name': product['name']}):
+        request_data = request.json["product"]
+        cached_products = get_cached_products()
+        product = find_product_from_cache("name", request_data["name"])
+
+        if product is not None:
             return jsonify({"message": "The resource already exists"}), 409
+        
+        result = products_collection.insert_one(request_data)
+        request_data["id"] = str(result.inserted_id)
+        del request_data["_id"]
+        if cached_products is None:
+            cached_products = [request_data]
         else:
-            products_collection.insert_one(product)
-            return jsonify({"message": "Succesfully added"}), 200
+            cached_products.append(request_data)
+        redis_client.set('products', json.dumps(cached_products))
+        return jsonify({"message": "Succesfully added"}), 200
+
     except Exception as e:
+        print("the exception", e)
         return jsonify({"message" : "Something went wrong"}), 404
 
 @app.route("/product/<id>", methods=["DELETE"])
 @log_request
 def delete_product(id):
     try:
-        product = get_product_by_id(id)
-        if (product is not None):
-            products_collection.delete_many({"_id": ObjectId(id)})
-            return jsonify({"message": "Succesfully removed"}), 200
-        return jsonify({"message": "The item was not found"}), 404
+        product = find_product_from_cache("id", id)
+        if product is None:
+            return jsonify({"message": "The item was not found"}), 404
+        
+        filtered_products = delete_product_from_cache(id)
+        products_collection.delete_many({"_id": ObjectId(id)})
+        redis_client.set("products", json.dumps(filtered_products))
+        return jsonify({"message": "Succesfully removed"}), 200
+    
     except Exception as e:
+        print(e)
         return jsonify({"message" : "Something went wrong"}), 404
     
 @app.route("/product/<id>", methods=["PUT"])
@@ -246,12 +294,15 @@ def delete_product(id):
 def update_product(id):
     try:
         update_data = request.json["product"]
-        product = get_product_by_id(id)
-        if (product is not None):
-            products_collection.update_many({'_id': ObjectId(id)}, {'$set': update_data})
-                    
-            return jsonify({"message": "Succesfully updated"}), 200
-        return jsonify({"message": "The item was not found"}), 404
+        product = find_product_from_cache("id", id)
+        if product is None:
+            return jsonify({"message": "The item was not found"}), 404
+        
+        products_collection.update_many({'_id': ObjectId(id)}, {'$set': update_data})
+        update_cached_products(id, update_data)
+        return jsonify({"message": "Succesfully updated"}), 200
+        
     except Exception as e:
+        print("the exception", e)
         return jsonify({"message": "Something went wrong"}), 404
     
